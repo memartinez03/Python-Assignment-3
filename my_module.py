82cbe34a60917e0e3f6faffb8734c634a6c3cfdf
memartinez03 <106893983+memartinez03@users.noreply.github.com>
2023-03-31 22:21:37 -0700

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,26 @@
+import math
+
+def calculate_square_footage(length, width):
+    """
+    Calculates the square footage of a house.
+
+    Args:
+        length (float): The length of the house in feet.
+        width (float): The width of the house in feet.
+
+    Returns:
+        float: The square footage of the house.
+    """
+    return length * width
+
+def calculate_circle_circumference(radius):
+    """
+    Calculates the circumference of a circle.
+
+    Args:
+        radius (float): The radius of the circle in feet.
+
+    Returns:
+        float: The circumference of the circle.
+    """
+    return 2 * math.pi * radius
